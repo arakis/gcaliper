cc5a4d35c34b448bd40190db9f6123c259e458b8
Sebastian Loncar <sebastian.loncar@gmail.com>
2023-05-01 21:12:23 +0200

    changed setup version

--- publish.ps1
+++ publish.ps1
@@ -7,7 +7,7 @@ dotnet publish -c Release -r win-x64 /p:PublishWindows=true
 dotnet publish -c Release -r linux-x64
 
 #set version=1.1.2
-& "C:\Program Files (x86)\Inno Setup 6\iscc.exe" contrib/win/gcaliper.iss /DMyConfiguration=Release /DMyConfiguration=Release /DMyAppVersion=1.1.2
+& "C:\Program Files (x86)\Inno Setup 6\iscc.exe" contrib/win/gcaliper.iss /DMyConfiguration=Release /DMyConfiguration=Release /DMyAppVersion=1.2.1
 
 $compress = @{
     Path = "src\bin\Release\net7.0\win-x64\publish\*"
